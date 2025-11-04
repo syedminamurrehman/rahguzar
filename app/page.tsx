aa669d7d2129d58fd3932486acc483a83252ce88
Syed Minam Ur Rehman <minamrahman727@gmail.com>
2025-11-04 12:25:55 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import NotificationBanner from "./components/banner";
 import Routes from "./components/Routes";
 import Footer from "./components/Footer";
+import ServiceWorkerRegister from "./components/servicecard";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <NotificationBanner />
       <Routes />
       <Footer/>
+      <ServiceWorkerRegister/>
     </div>
   );
 }
